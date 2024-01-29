862ca3eafd5ac57c04be65e5da71e099eef31422
Lahoucine Ijioui <ije1work@gmail.com>
2024-01-29 14:39:00 +0100

    get task by id route/controller

--- apps/mern-todolist-api/src/main.js
+++ apps/mern-todolist-api/src/main.js
@@ -6,18 +6,16 @@
 import express from 'express';
 import * as path from 'path';
 import notFoundError from './middlewares/notFoundError';
+import tasksRoutes from './routes/tasks.routes.';
 
 
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(express.json());
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to mern-todolist-api!' });
-});
-
-
-
+// tasks
+app.use('/api/tasks', tasksRoutes);
 
 // handle non existing routes
 app.use(notFoundError);
