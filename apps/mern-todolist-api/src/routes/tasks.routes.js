ff27844e44492ff8c196a75baa0154edfef69f96
Lahoucine Ijioui <ije1work@gmail.com>
2024-01-30 10:15:24 +0100

    update task route and controller / getAllTasks route and controller

---
+++ apps/mern-todolist-api/src/routes/tasks.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+const router = express.Router();
+import tasksControllers from "../controllers/tasks.controllers";
+
+// GET @/api/tasks get all tasks
+router.get('/', tasksControllers.getAllTasks);
+
+// POST @/api/tasks create a task
+router.post('/', tasksControllers.createTask)
+
+// GET @/api/tasks/id get task by id
+router.get('/:id', tasksControllers.getTaskById);
+
+// PUT @/api/tasks update the task
+router.put('/:id', tasksControllers.updateTask);
+
+export default router;
