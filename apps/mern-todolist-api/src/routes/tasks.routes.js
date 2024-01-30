cd30e240443c37b84d943669727fdb5aaf420d64
laitouss-yassine <yassinelaitouss2380@gmail.com>
2024-01-30 12:37:35 +0100

    resolve some issues

--- apps/mern-todolist-api/src/routes/tasks.routes.js
+++ apps/mern-todolist-api/src/routes/tasks.routes.js
@@ -1,18 +1,13 @@
 import express from "express";
-const router = express.Router();
+const )router = express.Router();
 import tasksControllers from "../controllers/tasks.controllers";
 
 
 // POST Tasks
 router.post('/', tasksControllers.createTask);
 
-
 // GET @/api/tasks/id get task by id
 router.get('/:id', tasksControllers.getTaskById);
-
-// delete task by id
-router.delete('/:id', tasksControllers.deleteTask);
-
 
 // GET @/api/tasks get all tasks
 router.get('/', tasksControllers.getAllTasks);
@@ -26,7 +21,8 @@ router.get('/:id', tasksControllers.getTaskById);
 // PUT @/api/tasks update the task
 router.put('/:id', tasksControllers.updateTask);
 
-
+// DELETE @/api/tasks/id delete the task
+router.delete('/:id', tasksControllers.deleteTask);
 export default router;
 
 
