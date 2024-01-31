1f6d9b795c17d197886dfbf6478e9bcf6c5b0fd5
laitouss-yassine <yassinelaitouss2380@gmail.com>
2024-01-31 14:23:02 +0100

    add tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  // Other Tailwind CSS configuration...
+
+  // Specify the content sources
+  content: [
+    "./apps/mern-todolist/**/*.{js,ts,jsx,tsx}", // Include JavaScript and TypeScript files
+    "./apps/**/*.html",          // Include HTML files
+  ],
+
+  // Other Tailwind CSS configuration...
+};
