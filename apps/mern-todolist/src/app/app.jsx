d8433de3c8904bace2b37f7c0a3ba1086a922342
Lahoucine Ijioui <ije1work@gmail.com>
2024-01-31 11:12:11 +0100

    establish react structure

--- apps/mern-todolist/src/app/app.jsx
+++ apps/mern-todolist/src/app/app.jsx
@@ -1,11 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import Home from '../pages/Home.jsx';
+import { Route, Routes } from 'react-router-dom';
+import TasksMain from '../pages/TasksMain.jsx';
 
 
-export function App() {
+function App() {
   return (
     <div>
-      <h1>hello world</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/tasks' element={<TasksMain />} />
+      </Routes>
     </div>
   );
 }
--- apps/mern-todolist/src/main.jsx
+++ apps/mern-todolist/src/main.jsx
@@ -1,12 +1,17 @@
+import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import React from 'react';
-
 
 import App from './app/app';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
 );
